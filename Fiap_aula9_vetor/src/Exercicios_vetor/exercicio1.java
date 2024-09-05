8c58be0404cc5779c7f4f502ef8ac5def5e2b42d
Yan-Wendel <yanwendelrodriguesmachado@gmail.com>
2024-09-05 15:02:43 -0300

    ex

---
+++ Fiap_aula9_vetor/src/Exercicios_vetor/exercicio1.java
@@ -0,0 +1,10 @@
+package Exercicios_vetor;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Fiap_aula9_vetor/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Fiap_aula9_vetor {
+}
